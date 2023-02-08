d909a44acf533cfe4bcb3ac8162b76258f5ace35
Sersoc <45021157+Sersoc@users.noreply.github.com>
2023-02-08 14:23:37 +0900

    create folder

---
+++ stack/stack/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  stack
+//
+//  Created by 이재익 on 2023/02/08.
+//
+
+#include <iostream>
+using namespace std;
+template<typename T>
+class stack{
+private:
+    T data;
+    stack<T>* next;
+};
+void pop(){
+    
+}
+template<typename t>
+void push(t item){
+    
+}
+void peek(){
+    
+}
+
+bool isEmpty(){
+
+    return true;
+}
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    return 0;
+}
